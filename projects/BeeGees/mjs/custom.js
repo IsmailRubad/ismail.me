40572e89b7cf3d46397138c57e5e13edcc838614
ismailrubad <rubad.hossain@gmail.com>
2018-05-28 21:46:07 +0600

    Add files via upload

---
+++ projects/BeeGees/mjs/custom.js
@@ -0,0 +1,212 @@
+/*global $,WOW*/
+/*
+===========================================================================
+ EXCLUSIVE ON themeforest.net
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ Project Name       : BeeGees - MultiPurpose Responsive template
+ Author             : Ismail Bin Dawud
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ Copyright (c) 2017 -  Ismail Bin Dawud - https://themeforest.net/user/nivodev
+===========================================================================
+*/
+
+
+"use strict";
+
+
+$(function(){
+
+	// ======================================
+	// Change carousel interval time
+	// ======================================
+
+	// interval is in milliseconds. 1000 = 1 second - so 1000 * 3 = 3 seconds
+    $("#myCarousel").carousel({interval: 1000 * 3});
+    $("#myCarousel2").carousel({interval: 1000 * 3});
+    $("#myCarousel3").carousel({interval: 1000 * 3});
+
+
+    // =================================
+	// Change header
+	// =================================
+    
+	// Cache reference to window
+	var $window = $(window);
+
+	$window.on('scroll', switchHeader);
+	function switchHeader(){
+		if ($window.scrollTop()==0) {
+			initHeader();
+			$('.go_top').removeClass('show');
+		}
+		else{
+			changeHeader();
+			$('.go_top').addClass('show');
+		}
+	}
+
+	function initHeader(){
+		$(".navbar-inverse .navbar-nav>li>a,#header .navbar-brand a,#header .social_link li a").css({"color": "#fff"});
+		
+		// logo size change
+		$("#header .navbar-brand a").css({"font-size": "40px"});
+
+		$(".navbar-inverse .navbar-toggle .icon-bar").css({"background-color": "#fff"});
+		
+		$("#header nav").removeClass("changeHeader");
+		$("#header nav").addClass("initHeader");
+		$("#header .social_link").removeClass("social_link_scroll");
+	}
+
+	function changeHeader(){
+		$(".navbar-inverse .navbar-nav>li>a,#header .navbar-brand a,#header .social_link li a").
+		css({"color": "#000"});
+
+		// logo initial size
+		$("#header .navbar-brand a").css({"font-size": "30px"});
+
+		$(".navbar-inverse .navbar-toggle .icon-bar").css({"background-color": "#000"});
+
+		$("#header nav").addClass("changeHeader");
+		$("#header .social_link").addClass("social_link_scroll");
+	}
+
+
+	// =================================
+	// Expertise cicle animation
+	// =================================
+
+	$('.circle').appear(function () {
+		
+		$('.circle').circleProgress();
+
+	});
+
+
+	// =================================
+	// number-counter animation
+	// =================================
+	$("#number_counter").appear(function () {
+		// count to animation
+		$(".timer").each(function () {
+			var e = $(this).attr("data-to");
+			$(this).delay(6e3).countTo({
+				from: 50,
+				to: e,
+				speed: 3e3,
+				refreshInterval: 50
+			})
+		})
+	});
+
+
+	// ======================================
+	// Projects filter
+	// ======================================
+
+    var $container = $('.portfolioContainer');
+    $container.isotope({
+        filter: '*',
+        animationOptions: {
+            duration: 750,
+            easing: 'linear',
+            queue: false
+        }
+    });
+ 
+    $('.portfolioFilter a').click(function(){
+        $('.portfolioFilter .current').removeClass('current');
+        $(this).addClass('current');
+ 
+        var selector = $(this).attr('data-filter');
+        $container.isotope({
+            filter: selector,
+            animationOptions: {
+                duration: 750,
+                easing: 'linear',
+                queue: false
+            }
+         });
+         return false;
+    });
+
+
+	
+
+
+	// ================================
+	//	Same page menu link
+	// ================================
+
+	// Cache selectors
+	var lastId,
+	    topMenu = $("#top_menu"),
+	    topMenuHeight = topMenu.outerHeight()+15,
+	    // All list items
+	    menuItems = topMenu.find("a"),
+	    // Anchors corresponding to menu items
+	    scrollItems = menuItems.map(function(){
+	    	var item = $($(this).attr("href"));
+	    	if (item.length) { return item; }
+	    });
+
+	// Bind click handler to menu items
+	// so we can get a fancy scroll animation
+
+	menuItems.on('click', function (e) {
+        var href = $(this).attr("href"),
+		    offsetTop = href === "#" ? 0 : $(href).offset().top-topMenuHeight+1;
+		$('html, body').stop().animate({ 
+		  scrollTop: offsetTop
+		}, 300);
+		e.preventDefault();
+    });
+
+
+	// Bind to scroll
+	$window.scroll(function(){
+	   // Get container scroll position
+	   var fromTop = $(this).scrollTop()+topMenuHeight;
+	   
+	   // Get id of current scroll item
+	   var cur = scrollItems.map(function(){
+	       if ($(this).offset().top < fromTop)
+	       return this;
+	   });
+	   // Get the id of the current element
+	   cur = cur[cur.length-1];
+	   var id = cur && cur.length ? cur[0].id : "";
+	   
+	   if (lastId !== id) {
+	       lastId = id;
+	       // Set/remove active class
+	       if(id=="home_sect"){
+	       		menuItems.parent().removeClass("active");
+	       }
+	       else{
+	       		menuItems
+	         		.parent().removeClass("active")
+	         		.end().filter("[href='#"+id+"']").parent().addClass("active");
+	       }
+	   }                   
+	});
+
+
+	
+	// ================================
+	//	Go back to top
+	// ================================
+
+	$('.go_top').on('click', function (e) {
+        e.preventDefault();
+        $('html,body').animate({
+            scrollTop: 0
+        }, 700);
+    });
+		
+});
+
+
+
+
+
